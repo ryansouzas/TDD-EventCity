8a9a3cea71cfaf0b5bb747e47bc0a44a491c8d5b
Ryan Souza <ryanssebadelhe@gmail.com>
2026-02-10 07:40:24 -0300

    City Layers

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,9 @@
+package com.devsuperior.bds02.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityService {
+
+}
