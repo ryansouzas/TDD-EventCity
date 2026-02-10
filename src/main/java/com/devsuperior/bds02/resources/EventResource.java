73036ff45822f01046ab2bb0abfc39d60b0e3d18
Ryan Souza <ryanssebadelhe@gmail.com>
2026-02-10 09:50:11 -0300

    Finished Project

--- src/main/java/com/devsuperior/bds02/resources/EventResource.java
+++ src/main/java/com/devsuperior/bds02/resources/EventResource.java
@@ -1,4 +1,22 @@
 package com.devsuperior.bds02.resources;
 
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/events")
 public class EventResource {
+
+    @Autowired
+    private EventService eventService;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto){
+        dto = eventService.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
 }
